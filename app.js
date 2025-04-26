1b66bccc855094744078221e5842a403d25bbe83
01mehran <132054519+01mehran@users.noreply.github.com>
2025-04-26 14:28:54 +0330

    Add sound effect

--- app.js
+++ app.js
@@ -37,4 +37,22 @@ const Time = () => {
 };
 setInterval(Time, 1000);
 
+// ----------- Play Clock Sound Effect;
+let soundInterval = null;
+let isSoundOn = false;
+container.addEventListener("click", () => {
+  isSoundOn = !isSoundOn;
 
+  if (isSoundOn) {
+    if (soundInterval) {
+      clearInterval(soundInterval);
+    }
+    soundInterval = setInterval(() => {
+      const clockSound = new Audio("./sound.wav");
+      clockSound.play();
+    }, 1000);
+  } else if (soundInterval) {
+    clearInterval(soundInterval);
+    soundInterval = null;
+  }
+});
